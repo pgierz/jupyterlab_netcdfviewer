da9461fb5bc08af6865ed9b102d30e7cd102215e
Paul Gierz <paulgierz@gmail.com>
2018-03-21 10:47:21 -0700

    script now tests if variables are valid with the netcdf json variable definition I wrote

--- standalone_examples/netcdf_reader.js
+++ standalone_examples/netcdf_reader.js
@@ -15,6 +15,9 @@ Dr. Paul Gierz, AWI Bremerhaven
 const fs = require('fs');
 const NetCDFReader = require('netcdfjs');
 
+var Validator = require('jsonschema').Validator;
+var v = new Validator();
+
 // There are some example files that can be downloaded here:
 // http://www.unidata.ucar.edu/software/netcdf/examples/files.html
 
@@ -26,41 +29,66 @@ var reader = new NetCDFReader(data);
 // figuring out how to do some basics.
 
 // NOTE: Print stuff with console.log
-console.log("The reader has the following methods (these are called "+"properties".bold()+"in Javascript)");
-console.log(Object.getOwnPropertyNames(reader)); // ['header', 'buffer']
-
-// NOTE: Access attributes (sub-objects? what exactly are these called) with the "."
-// method, just like in python
+// console.log("The reader has the following methods (these are called "+"properties".bold()+"in Javascript)");
+// console.log(Object.getOwnPropertyNames(reader)); // ['header', 'buffer']
 //
-// Let's look at the header:
-console.log("Here's the header...");
-console.log(reader.header);
-console.log("...done!");
-
-// Can you look at the variables like in python?
-console.log("Here are the variables...");
-console.log(reader.variables);
-console.log("...done!");
-
-// Now we want to figure out what the type of the reader.variables is..
-const typeVariables = Object.prototype.toString.call(reader.variables);
-console.log("The type of reader.variables is", typeVariables);
-
-// NOTE: The next bit contains an example of how to loop over an array:
+// // NOTE: Access attributes (sub-objects? what exactly are these called) with the "."
+// // method, just like in python
+// //
+// // Let's look at the header:
+// console.log("Here's the header...");
+// console.log(reader.header);
+// console.log("...done!");
 //
-// How about just one specific variable?
-console.log("Here's are all the names of the specific variables: ");
-for (var i = 0; i < reader.variables.length; i++){
-  console.log(i, reader.variables[i].name);
+// // Can you look at the variables like in python?
+// console.log("Here are the variables...");
+// console.log(reader.variables);
+// console.log("...done!");
+
+// // Now we want to figure out what the type of the reader.variables is..
+// const typeVariables = Object.prototype.toString.call(reader.variables);
+// console.log("The type of reader.variables is", typeVariables);
+//
+// // NOTE: The next bit contains an example of how to loop over an array:
+// //
+// // How about just one specific variable?
+// console.log("Here's are all the names of the specific variables: ");
+// for (var i = 0; i < reader.variables.length; i++){
+//   console.log(i, reader.variables[i].name);
+// }
+//
+// // NOTE: The reader.variables object is an ARRAY! This means that you can't get
+// // a key out of it like you would be able to do with a dictionary in python.
+// // Instead, you need to give the actual index number. Let's see if we can
+// //
+// // actually get down to the array of a specific variable:
+//console.log(typeof(reader.variables[0]))
+//console.log(reader.variables);
+
+let variable_schema = {
+  "id": "/SimpleVariable",
+  "type": "object",
+  "properties": {
+    "name": {"type": "string"},
+    "dimensions": {"type": "array",
+                  "items": { "type": "number"}
+                },
+    "attributes": {"type": "array",
+                   "items": {"type": "object"}},
+    "type": {"type": "string"},
+    "size": {"type": "number"},
+    "offset": {"type": "number"},
+    "record": {"type": "boolean"}
+  }
 }
 
-// NOTE: The reader.variables object is an ARRAY! This means that you can't get
-// a key out of it like you would be able to do with a dictionary in python.
-// Instead, you need to give the actual index number. Let's see if we can
-//
-// actually get down to the array of a specific variable:
-const tas_attributes = reader.variables[8].attributes
-console.log(tas_attributes);
-// That just prints out the tas_attributes
-const tasArray = reader.getDataVariable("tas")
-console.log(tasArray);
+for (var i = 0; i < reader.variables.length; i++){
+  let result = v.validate(reader.variables[0], variable_schema);
+  console.log(result.valid)
+}
+
+
+
+// // That just prints out the tas_attributes
+// const tasArray = reader.getDataVariable("tas")
+// console.log(tasArray);
